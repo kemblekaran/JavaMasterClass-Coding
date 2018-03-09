62b416e0b75422335dbf82ae898c3adead1b8732
kemblekaran <kemblekaran@gmail.com>
2018-03-09 22:29:59 +0530

    forloops implemeted

--- src/io/github/kemblekaran/Main.java
+++ src/io/github/kemblekaran/Main.java
@@ -3,6 +3,5 @@ package io.github.kemblekaran;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
     }
 }
---
+++ src/io/github/kemblekaran/forloops/ForLoops.java
@@ -0,0 +1,32 @@
+package io.github.kemblekaran.forloops;
+
+public class ForLoops {
+    private final static int START_INTEREST_RATE = 2;
+    private final static int END_INTEREST_RATE = 8;
+
+    public static void main(String[] args) {
+        ForLoops forLoops = new ForLoops();
+        forLoops.calculateMultipleInterestRates(10000.0);
+    }
+
+    /**
+     * Calculate the interest rate here
+     * @param amount
+     * @param interestRate
+     * @return
+     */
+    public static double calculateInterest(double amount, double interestRate) {
+        return amount * (interestRate / 100);
+    }
+
+    /**
+     * calculates multiple interest rate range between START_INTEREST_RATE to END_INTEREST_RATE value
+     * and the formats it to have two decimal value
+     * @param amount
+     */
+    public void calculateMultipleInterestRates(double amount) {
+        for (int i = START_INTEREST_RATE; i <= END_INTEREST_RATE; i++) {
+            System.out.println(i+"% Interest Rate for the amount 10000.0 is " + String.format("%.2f",calculateInterest(amount, i)));
+        }
+    }
+}
